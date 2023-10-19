95d6b743fa825dffe68c0a084ee5cc22a5c77288
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2023-10-19 19:01:57 +0600

    add navbar

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
 
 
 const Root = () => {
     return (
         <div>
+        <div className="max-w-screen-xl mx-auto">
+        <Navbar ></Navbar>
+        </div>
+      
            <Outlet></Outlet> 
         </div>
     );
